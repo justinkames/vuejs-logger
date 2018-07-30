ad95ac1f31b441ffe9679b8db89c1ab9c586f67a
Justin Kames <justinkames@gmail.com>
2018-07-30 15:45:02 +0200

    Added jest.config.js. Updated tsconfig.json. Updated package.json.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
-import VueLogger from "./vue-logger";
-
+import ObjectAssign from "es6-object-assign";
+ObjectAssign.polyfill();
+import VueLogger from "./vue-logger/vue-logger";
 export default VueLogger;
